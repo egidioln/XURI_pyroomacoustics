13442290538bcdfae5489929966cae24da94dab3
Lucas Neves Egidio <lucas.egidio@liu.se>
2020-10-01 18:19:24 +0200

    fix setup 2

--- pyroomacoustics-master/setup.py
+++ pyroomacoustics-master/setup.py
@@ -176,7 +176,7 @@ setup_kwargs = dict(
     # Libroom C extension
     ext_modules=ext_modules,
     # Necessary to keep the source files
-    package_data={"pyroomacoustics": ["*.pxd", "*.pyx", mypath+"data/materials.json"]},
+    package_data={mypath+"pyroomacoustics": ["*.pxd", "*.pyx", mypath+"data/materials.json"]},
     install_requires=["Cython", "numpy", "scipy>=0.18.0", "pybind11>=2.2",],
     cmdclass={"build_ext": BuildExt},  # taken from pybind11 example
     zip_safe=False,
